25113dea43f051636f4cac29c1cd5647f7fd26e0
GitHub Action <action@github.com>
2023-06-23 19:16:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/sergeysenja1992/includehttpclient/MyBundle.kt
+++ src/main/kotlin/com/github/sergeysenja1992/includehttpclient/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sergeysenja1992.includehttpclient
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sergeysenja1992/includehttpclient/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sergeysenja1992/includehttpclient/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sergeysenja1992.includehttpclient.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
